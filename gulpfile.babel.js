7d6d2cb55111a6ffe029ffd2fd8a3b8fc72bb11e
Daniel Leite de Oliveira <dleitee@gmail.com>
2015-09-27 12:15:28 -0300

    New gulp file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,6 +6,25 @@
 *
 */
 
-import gulp from 'gulp';
+import gulp from 'gulp'
+import browserify from 'browserify'
+import babelify from 'babelify'
+import source from 'vinyl-source-stream'
+import uglify from 'gulp-uglify'
+import buffer from 'vinyl-buffer'
 
+
+gulp.task('js', () => {
+  browserify({
+    entries: './src/validate.js',
+    extensions: ['.js'],
+    debug: true
+  })
+  .transform(babelify)
+  .bundle()
+  .pipe(source('bundle.js'))
+  .pipe(buffer())
+  .pipe(uglify())
+  .pipe(gulp.dest('dist'));
+})
 
