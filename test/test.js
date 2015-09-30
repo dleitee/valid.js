ed9032bc09f9550b62391f34fb5a2066eb83e210
Daniel Leite de Oliveira <dleitee@gmail.com>
2015-09-30 12:39:19 -0300

    Tests

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 import expect from 'expect.js'
-import {validate, string} from '../src/validate'
+import {validate, string, number} from '../src/validate'
 
 describe('Library Validate', () => {
 
@@ -10,6 +10,10 @@ describe('Library Validate', () => {
       expect(string.minLength).to.be.a('function')
       expect(string.maxLength).to.be.a('function')
       expect(string.hasString).to.be.a('function')
+      expect(number.maxNumber).to.be.a('function')
+      expect(number.minNumber).to.be.a('function')
+      expect(number.between).to.be.a('function')
+      expect(number.isNumber).to.be.a('function')
     })
 
   })
@@ -17,21 +21,21 @@ describe('Library Validate', () => {
   describe('string#minLength', () => {
 
     it('should be true for all', () => {
-        expect(validate('', string.minLength(0))).to.be.ok()
-        expect(validate('1', string.minLength(0))).to.be.ok()
-        expect(validate('1', string.minLength(1))).to.be.ok()
-        expect(validate('', string.minLength(-1))).to.be.ok()
+        expect(validate(string.minLength(0))('')).to.be.ok()
+        expect(validate(string.minLength(0))('1')).to.be.ok()
+        expect(validate(string.minLength(1))('1')).to.be.ok()
+        expect(validate(string.minLength(-1))('')).to.be.ok()
     })
 
     it('should be false for all', () => {
-      expect(validate('1', string.minLength(2))).to.not.be.ok()
-      expect(validate(null, string.minLength(2))).to.not.be.ok()
-      expect(validate(undefined, string.minLength(2))).to.not.be.ok()
-      expect(validate(1, string.minLength(2))).to.not.be.ok()
-      expect(validate(1.2, string.minLength(2))).to.not.be.ok()
-      expect(validate([], string.minLength(2))).to.not.be.ok()
-      expect(validate(['a', 'b'], string.minLength(2))).to.not.be.ok()
-      expect(validate({}, string.minLength(2))).to.not.be.ok()
+      expect(validate(string.minLength(2))('1')).to.not.be.ok()
+      expect(validate(string.minLength(2))(null)).to.not.be.ok()
+      expect(validate(string.minLength(2))(undefined)).to.not.be.ok()
+      expect(validate(string.minLength(2))(1)).to.not.be.ok()
+      expect(validate(string.minLength(2))(1.2)).to.not.be.ok()
+      expect(validate(string.minLength(2))([])).to.not.be.ok()
+      expect(validate(string.minLength(2))(['a', 'b'])).to.not.be.ok()
+      expect(validate(string.minLength(2))({})).to.not.be.ok()
     })
 
   })
