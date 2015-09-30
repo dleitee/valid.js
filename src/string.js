91097d5c726bd6c54e1dee0804e84183fa8bfb1f
Daniel Leite de Oliveira <dleitee@gmail.com>
2015-09-30 19:21:25 -0300

    test / string.js

--- src/string.js
+++ src/string.js
@@ -2,7 +2,7 @@ import {isNil} from './lib/isnil.js'
 import {_isString} from './lib/string'
 
 export const string = {
-    hasString: (string) => _isString(string),
+    isString: (string) => _isString(string),
     minLength: (value) =>
                     (string) => {
                         if(isNil(value))
@@ -10,7 +10,13 @@ export const string = {
                         else
                             return _isString(string)?string.length >= value: false
                     },
-    maxLength: (value) => (string) => _isString(string)?string.length <= value: false
+    maxLength: (value) =>
+                    (string) => {
+                        if(isNil(value))
+                            throw new Error('The value for maxLength is null or undefined.')
+                        else
+                            return _isString(string)?string.length <= value: false
+                    }
 }
 
 
--- test/string.test.js
+++ test/string.test.js
@@ -6,43 +6,126 @@ describe('Functions String', () => {
   describe('#minLength(value)', () => {
 
     it('should be true when string.length >=', () => {
-        let result = validate(string.minLength(1))
-        expect(result('T')).to.be.ok()
+      let result = validate(string.minLength(1))
+      expect(result('T')).to.be.ok()
     })
 
     it('should be false when string.length <', () => {
-        let result = validate(string.minLength(1))
-        expect(result('')).to.not.be.ok()
+      let result = validate(string.minLength(1))
+      expect(result('')).to.not.be.ok()
     })
 
     it('should be false when value is null', () => {
-        let result = validate(string.minLength(1))
-        expect(result(null)).to.not.be.ok()
+      let result = validate(string.minLength(1))
+      expect(result(null)).to.not.be.ok()
     })
 
     it('should be false when value is undefined', () => {
-        let result = validate(string.minLength(1))
-        expect(result(undefined)).to.not.be.ok()
+      let result = validate(string.minLength(1))
+      expect(result(undefined)).to.not.be.ok()
     })
 
     it('should be false when value is array', () => {
-        let result = validate(string.minLength(1))
-        expect(result([])).to.not.be.ok()
+      let result = validate(string.minLength(1))
+      expect(result([])).to.not.be.ok()
     })
 
     it('should be false when value is object', () => {
-        let result = validate(string.minLength(1))
-        expect(result({})).to.not.be.ok()
+      let result = validate(string.minLength(1))
+      expect(result({})).to.not.be.ok()
     })
 
     it('It should be an exception when undefined', () => {
-        let result = validate(string.minLength(null))
-        expect(result.bind(this, 'Test')).to.throwError()
+      let result = validate(string.minLength(null))
+      expect(result.bind(this, 'Test')).to.throwError()
     })
 
     it('It should be an exception when undefined', () => {
-        let result = validate(string.minLength(undefined))
-        expect(result.bind(this, 'Test')).to.throwError()
+      let result = validate(string.minLength(undefined))
+      expect(result.bind(this, 'Test')).to.throwError()
+    })
+
+  })
+
+  describe('#maxLength(value)', () => {
+
+    it('should be true when string.length <=', () => {
+      let result = validate(string.maxLength(1))
+      expect(result('T')).to.be.ok()
+    })
+
+    it('should be true when string.length <=', () => {
+      let result = validate(string.maxLength(1))
+      expect(result('')).to.be.ok()
+    })
+
+    it('should be false when string.length >', () => {
+      let result = validate(string.maxLength(1))
+      expect(result('Te')).to.not.be.ok()
+    })
+
+    it('should be false when value is null', () => {
+      let result = validate(string.maxLength(1))
+      expect(result(null)).to.not.be.ok()
+    })
+
+    it('should be false when value is undefined', () => {
+      let result = validate(string.maxLength(1))
+      expect(result(undefined)).to.not.be.ok()
+    })
+
+    it('should be false when value is array', () => {
+      let result = validate(string.maxLength(1))
+      expect(result([])).to.not.be.ok()
+    })
+
+    it('should be false when value is object', () => {
+      let result = validate(string.maxLength(1))
+      expect(result({})).to.not.be.ok()
+    })
+
+    it('It should be an exception when undefined', () => {
+      let result = validate(string.maxLength(null))
+      expect(result.bind(this, 'Test')).to.throwError()
+    })
+
+    it('It should be an exception when undefined', () => {
+      let result = validate(string.maxLength(undefined))
+      expect(result.bind(this, 'Test')).to.throwError()
+    })
+
+  })
+
+  describe('#isString(string)', () => {
+
+    it('should be true when a string', () => {
+      let result = validate(string.isString)
+      expect(result('Test')).to.be.ok()
+    })
+
+    it('should be false when an array', () => {
+      let result = validate(string.isString)
+      expect(result([])).to.not.be.ok()
+    })
+
+    it('should be false when an object', () => {
+      let result = validate(string.isString)
+      expect(result({})).to.not.be.ok()
+    })
+
+    it('should be false when a number', () => {
+      let result = validate(string.isString)
+      expect(result(1)).to.not.be.ok()
+    })
+
+    it('should be false when value is null', () => {
+      let result = validate(string.isString)
+      expect(result(null)).to.not.be.ok()
+    })
+
+    it('should be false when value is undefined', () => {
+      let result = validate(string.isString)
+      expect(result(undefined)).to.not.be.ok()
     })
 
   })
