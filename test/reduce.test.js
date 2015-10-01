3b9639df3a699313db3f2d8ee78369c5a4117534
Daniel Leite de Oliveira <dleitee@gmail.com>
2015-10-01 15:28:27 -0300

    test/reduce

---
+++ test/reduce.test.js
@@ -0,0 +1,29 @@
+import {reduce} from '../src/lib/reduce.js'
+
+import expect from 'expect.js'
+
+describe('Library Reduce', () => {
+
+  describe('#reduce(func, index, array)', () => {
+
+    const add = (a, b) => a + b
+
+    it('should be 15 when array is [1,2,3,4,5]', () => {
+      let result = reduce(add, 0, [1,2,3,4,5])
+      expect(result).to.be(15)
+    })
+
+    it('It should be an exception when index is not an array', () => {
+      let result = reduce.bind(this, add, 0, null)
+      expect(result).to.throwError()
+    })
+
+    it('It should be an exception when func is not a function', () => {
+      let result = reduce.bind(this, undefined, 0, [1,2,3,4,5])
+      expect(result).to.throwError()
+    })
+
+  })
+
+
+})
