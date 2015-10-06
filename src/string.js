c9f9c1fb8ed637c5eba76ce495ebbb0a52bb4abd
Daniel Leite de Oliveira <dleitee@gmail.com>
2015-10-06 14:35:22 -0300

    Refactory

--- src/string.js
+++ src/string.js
@@ -1,24 +1,41 @@
 import {isNil} from './lib/isnil.js'
 import {_isString} from './lib/string'
 
+/*
+ * Checks for a 'String'
+ * @param string - value for check
+ * @return boolean
+ */
 const isString = (string) => _isString(string)
 
+/*
+ * Validates the size of the 'string' is less than or equal to (<=) the value
+ * @param minLength
+ * @param string - value for check
+ * @return boolean
+ */
 const minLength =
-        (value) =>
+        (minLength) =>
           (string) => {
-            if(isNil(value))
+            if(isNil(minLength))
               throw new Error('The value for minLength is null or undefined.')
             else
-              return _isString(string)?string.length >= value: false
+              return _isString(string)?string.length >= minLength: false
           }
 
+/*
+ * Validates the size of the 'string' is greater than or equal to (> =) the value
+ * @param maxLength
+ * @param string - value for check
+ * @return boolean
+ */
 const maxLength =
-        (value) =>
+        (maxLength) =>
           (string) => {
-            if(isNil(value))
+            if(isNil(maxLength))
               throw new Error('The value for maxLength is null or undefined.')
             else
-              return _isString(string)?string.length <= value: false
+              return _isString(string)?string.length <= maxLength: false
           }
 
 
