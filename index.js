481d52b50370d8ad3786b3d936913fd39c865ada
Daniel Leite de Oliveira <dleitee@gmail.com>
2015-10-21 15:48:42 -0200

    lint

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 var validatejs = require('./dist/validate')
-var validate = validatejs.validate,
-    string = validatejs.string,
-    util = validatejs.util
+var validate = validatejs.validate
+var string = validatejs.string
+var util = validatejs.util
 
 var isvalid = validate(string.maxLength(15), util.isRequired)
 var result = isvalid('Test ValidateJS')
